b14f9d5f409bc3afb6e6efe5b4e6ddf16485c1be
Daniel Kucal <dkucal@gmail.com>
2017-05-25 01:56:21 +0200

    feat(WebStorageService): add some comments, keys and config properties, make clear method more customisable

--- src/service/webstorage.service.ts
+++ src/service/webstorage.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { ClearType, Config } from '../config';
 import { WebStorageUtility } from '../utility/webstorage.utility';
+import { WebStorageConfigInterface } from '../config/config.interface';
 
 export interface WebStorageServiceInterface {
     keys: Array<string>;
     new(): {
+        keys: Array<string>;
+        config: WebStorageConfigInterface;
         get(key: string): any;
         set(key: string, value: any): void;
         remove(key: string): void;
@@ -16,6 +19,18 @@ export abstract class WebStorageService {
     public static keys: Array<string>;
 
     constructor(protected storage: Storage) {}
+
+    /**
+     * Gets keys from child class
+     * @returns {Array<string>}
+     */
+    public get keys(): Array<string> {
+        return (<WebStorageServiceInterface>this.constructor).keys;
+    }
+
+    public get config(): WebStorageConfigInterface {
+        return Config;
+    }
 
     public get(key: string): any {
         return WebStorageUtility.get(this.storage, key);
@@ -29,16 +44,21 @@ export abstract class WebStorageService {
         WebStorageUtility.remove(this.storage, key);
     }
 
-    public clear(clearType?: ClearType): void {
+    /**
+     * Clears chosen data from Storage
+     * @param clearType 'decorators', 'prefix' or 'all'
+     * @param prefix if clearType = prefix, defines the prefix
+     */
+    public clear(clearType?: ClearType, prefix?: string): void {
         clearType = clearType || Config.clearType;
-        let keys = (<WebStorageServiceInterface>this.constructor).keys; // get keys from child class
         if (clearType === 'decorators') {
-            for (let key of keys) {
+            for (let key of this.keys) {
                 this.storage.removeItem(key);
             }
         } else if (clearType === 'prefix') {
+            prefix = prefix || Config.prefix;
             for (let key in this.storage) {
-                if (this.storage.getItem(key).startsWith(Config.prefix)) {
+                if (this.storage.getItem(key).startsWith(prefix)) {
                     this.storage.removeItem(key);
                 }
             }
