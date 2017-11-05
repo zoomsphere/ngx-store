be1c835bf3f114d4aa324570542e0e0653e910a5
Daniel Kucal <dkucal@gmail.com>
2017-11-05 19:41:49 +0100

    feat(StorageEvent): implement StorageEvent interface with a bit compiler hacking

--- src/utility/storage/storage-event.ts
+++ src/utility/storage/storage-event.ts
@@ -1,4 +1,4 @@
-export class NgxStorageEvent {
+export class NgxStorageEvent implements StorageEvent {
     protected static initTimeStamp = Date.now();
     public oldValue?: any;
     public newValue: any;
@@ -13,12 +13,52 @@ export class NgxStorageEvent {
     public readonly isTrusted = true;
     public readonly path = [window];
     public readonly returnValue = true;
-    public readonly srcElement = window;
+    public readonly srcElement = <any>window;
     public readonly target = window;
     public readonly url = window.location.href;
     public readonly isInternal = true;
 
     constructor(public type: string, public key: string, public storageArea: Storage) {
-        Object.setPrototypeOf(this, StorageEvent);
+        setTimeout(() => Object.setPrototypeOf(this, new StorageEvent(type)));
+    }
+
+    /**
+     * Methods below exist only to satisfy TypeScript compiler
+     */
+
+    public get scoped() {
+        return undefined;
+    };
+
+    public get initEvent() {
+        return undefined;
+    };
+
+    public get preventDefault() {
+        return undefined;
+    }
+
+    public get stopImmediatePropagation() {
+        return undefined;
+    }
+
+    public get stopPropagation() {
+        return undefined;
+    }
+
+    public get deepPath() {
+        return undefined;
+    }
+
+    public get AT_TARGET() {
+        return undefined;
+    }
+
+    public get BUBBLING_PHASE() {
+        return undefined;
+    }
+
+    public get CAPTURING_PHASE() {
+        return undefined;
     }
 }
