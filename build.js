20fcf2d0d53a8b8cd31a193fbab6ae52d22fce75
Daniel Kucal <dkucal@gmail.com>
2018-11-28 20:34:56 +0100

    chore(build): enable minifying

--- build.js
+++ build.js
@@ -40,33 +40,32 @@ if (shell.exec(`rollup -c rollup.es.config.js -i ${NPM_DIR}/${PACKAGE}.js -o ${E
     shell.exit(1);
 }
 
-// Uncomment everything for Angular5
-// shell.echo(`Produce ESM5 version`);
-// shell.exec(`ngc -p tsconfig-build.json --target es5 -d false --outDir ${OUT_DIR_ESM5} --importHelpers true --sourceMap`);
-// if (shell.exec(`rollup -c rollup.es.config.js -i ${OUT_DIR_ESM5}/${PACKAGE}.js -o ${ESM5_DIR}/${PACKAGE}.js`).code !== 0) {
-//     shell.echo(chalk.red(`Error: ESM5 version failed`));
-//     shell.exit(1);
-// }
+shell.echo(`Produce ESM5 version`);
+shell.exec(`ngc -p tsconfig-build.json --target es5 -d false --outDir ${OUT_DIR_ESM5} --importHelpers true --sourceMap`);
+if (shell.exec(`rollup -c rollup.es.config.js -i ${OUT_DIR_ESM5}/${PACKAGE}.js -o ${ESM5_DIR}/${PACKAGE}.js`).code !== 0) {
+    shell.echo(chalk.red(`Error: ESM5 version failed`));
+    shell.exit(1);
+}
 
-// shell.echo(`Run Rollup conversion on package`);
-// if (shell.exec(`rollup -c rollup.config.js -i ${ESM5_DIR}/${PACKAGE}.js -o ${BUNDLES_DIR}/${PACKAGE}.umd.js`).code !== 0) {
-//     shell.echo(chalk.red(`Error: Rollup conversion failed`));
-//     shell.exit(1);
-// }
+shell.echo(`Run Rollup conversion on package`);
+if (shell.exec(`rollup -c rollup.config.js -i ${ESM5_DIR}/${PACKAGE}.js -o ${BUNDLES_DIR}/${PACKAGE}.umd.js`).code !== 0) {
+    shell.echo(chalk.red(`Error: Rollup conversion failed`));
+    shell.exit(1);
+}
 
-// shell.echo(`Minifying`);
-// shell.cd(`${BUNDLES_DIR}`);
-// shell.exec(`uglifyjs ${PACKAGE}.umd.js -c --comments -o ${PACKAGE}.umd.min.js --source-map "filename='${PACKAGE}.umd.min.js.map', includeSources"`);
-// shell.cd(`..`);
-// shell.cd(`..`);
+shell.echo(`Minifying`);
+shell.cd(`${BUNDLES_DIR}`);
+shell.exec(`uglifyjs ${PACKAGE}.umd.js -c --comments -o ${PACKAGE}.umd.min.js --source-map "filename='${PACKAGE}.umd.min.js.map', includeSources"`);
+shell.cd(`..`);
+shell.cd(`..`);
 
 shell.echo(chalk.green(`Bundling completed`));
 
 shell.rm(`-Rf`, `${NPM_DIR}/package`);
 shell.rm(`-Rf`, `${NPM_DIR}/node_modules`);
-// shell.rm(`-Rf`, `${NPM_DIR}/*.js`);
+shell.rm(`-Rf`, `${NPM_DIR}/*.js`);
 shell.rm(`-Rf`, `${NPM_DIR}/*.js.map`);
-// shell.rm(`-Rf`, `${NPM_DIR}/src/**/*.js`);
+shell.rm(`-Rf`, `${NPM_DIR}/src/**/*.js`);
 shell.rm(`-Rf`, `${NPM_DIR}/src/**/*.js.map`);
 
 shell.cp(`-Rf`, [`package.json`, `LICENSE`, `README.md`], `${NPM_DIR}`);
